bf4624bf01ec4abc46c956a01e5217fddd2fddd3
nafell <pixel68.43@gmail.com>
2025-05-23 18:02:14 +0900

    Revert nrp address to 127.0.0.1

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,7 @@ enum Commands {
     /// サーバーモードで起動します
     Server {
         /// サーバーがリッスンするUDPアドレス:ポート
-        #[arg(short, long, default_value = "0.0.0.0:12345")]
+        #[arg(short, long, default_value = "127.0.0.1:12345")]
         bind_addr: String,
         /// Web UIサーバーがリッスンするTCPアドレス:ポート
         #[arg(short, long, default_value = "0.0.0.0:8080")]
@@ -80,7 +80,7 @@ enum Commands {
     /// クライアントモードで起動し、メッセージを送信します
     Client {
         /// 接続先のサーバーアドレス:ポート
-        #[arg(short, long, default_value = "0.0.0.0:12345")]
+        #[arg(short, long, default_value = "127.0.0.1:12345")]
         server_addr: String,
         /// 送信するメッセージ
         #[arg(short, long, default_value = "Hello, protocol!")]
